8a8b68b8fcb3882989eaaabb3ac82100b19bebf3
ramrodwal <rrodwal22@gmail.com>
2023-03-01 06:58:31 +0530

    added navbar

---
+++ mernapp/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Home from './screens/Home';
+import{
+BrowserRouter as Router,
+Routes,
+Route,
+Link
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+    
+    <div> <Home/></div>
+    
+    </>
+  );
+}
+
+export default App;
