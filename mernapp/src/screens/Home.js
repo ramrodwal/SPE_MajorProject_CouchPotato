0d7890fe3c5b92b151d3e6e3b353dc11f39a8f0f
ramrodwal <rrodwal22@gmail.com>
2023-03-03 14:32:50 +0530

    added card component

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from '../components/Card'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
@@ -8,31 +9,7 @@ export default function
     <div>
 
       <div> <Navbar /> </div>
-
-      <div>
-
-        <div className="card mt-3" style={{ "width": "18rem", "maxHeight" : "360px" }}>
-          <img src="..." class="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">this is some text</p>
-            <div className='container w-100'>
-              <select className='m-2 h-100 bg-success rounded' >
-                {Array.from(Array(6), (e, i)=>{
-                  return(
-                    <option key={i+1} value={i+1}>{i+1}</option>
-                  )
-                })}
-              </select>
-              <select className='m-2 h-100 bg-success rounded'>
-                <option value="half">Half</option>
-                <option value="full">Full</option>
-              </select>
-            </div>
-          </div>
-        </div>
-
-      </div>
+      <div><Card></Card></div>
       <div><Footer /></div>
     </div>
   )
