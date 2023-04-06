1bd4ece1bb76bda5085fea921be07a780eb228f9
ramrodwal <rrodwal22@gmail.com>
2023-04-06 12:00:14 +0530

    created login and signup page working and navigating properly with valid and invalid credentials

--- mernapp/backend/Routes/CreateUser.js
+++ mernapp/backend/Routes/CreateUser.js
@@ -1,33 +1,70 @@
 const express = require('express')
-const router = express.Router() 
+const router = express.Router()
 const user = require('../models/User')
 const { body, validationResult } = require('express-validator');
 
 router.post("/createUser", [
-    body('email').isEmail(),
-    body('name').isLength({ min: 5 }),
+  body('email').isEmail(),
+  body('name').isLength({ min: 5 }),
   // password must be at least 5 chars long
   body('password', 'incorrect password').isLength({ min: 5 })
-], async(req, res)=>{
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+
+    await user.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+      location: req.body.location
+    })
+    res.json({ success: true });
+
+  } catch (error) {
+    console.log("there is an error");
+    res.json({ success: true });
+  }
+
+})
+
+
+router.post("/loginuser", [
+  body('email').isEmail(),
+  // password must be at least 5 chars long
+  body('password', 'incorrect password').isLength({ min: 5 })
+], async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  let email = req.body.email;
+
+  try {
+
+    let userData = await user.findOne({ email });
+    if (!userData) {
+      return res.status(400).json({ errors: "email not registered" });
     }
 
-    try {
-        
-       await user.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-            location: req.body.location
-        })
-        res.json({success:true});
-
-    } catch (error) {
-        console.log("there is an error");
-        res.json({success:true});
+    if (req.body.password !== userData.password) {
+      return res.status(400).json({ errors: "incorrect password" });
     }
+
+    else {
+      res.json({ success: true });
+    }
+
+
+  } catch (error) {
+    console.log("there is an error");
+    res.json({ success: false });
+  }
 
 })
 
--- mernapp/src/screens/Login.js
+++ mernapp/src/screens/Login.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
+
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  let navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    //this is a synthetic event i.e preventDefault
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/loginuser", {
+      method: 'POST',
+      //specified the header in thunderclient
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      //json data ko stringify krke bhejte hai kyuki yahi tarika hota hai
+      body: JSON.stringify({ email: credentials.email, password: credentials.password })
+    });
+
+    const json = await response.json()
+    console.log(json);
+
+    if (!json.success) {
+      alert("enter valid credentials")
+    }
+    else{
+      navigate("/");
+    }
+
+  }
+
+  const onChange = (event) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value })
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" name='password' value={credentials.password} onChange={onChange} id="exampleInputPassword1" />
+          </div>
+          <button type="submit" className="m-3 btn btn-success">Submit</button>
+          <Link to="/createuser" className='m-3 btn btn-danger'>I'm a new user</Link>
+        </form>
+
+      </div>
+    </div>
   )
 }
