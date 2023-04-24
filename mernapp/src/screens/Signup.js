3885e5e87db2a588686ecdcde418c55fe0fede09
ramrodwal <rrodwal22@gmail.com>
2023-04-24 14:09:57 +0530

    updated signup page UI

--- mernapp/src/screens/Signup.js
+++ mernapp/src/screens/Signup.js
@@ -1,71 +1,122 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
+
 export default function Signup() {
+  const navigate = useNavigate();
 
-  const navigate= useNavigate();
-
-  const [credentials, setCredentials] = useState({ name: "", email: "", password: "", geolocation: "" });
+  const [credentials, setCredentials] = useState({
+    name: '',
+    email: '',
+    password: '',
+    geolocation: ''
+  });
 
   const handleSubmit = async (e) => {
-    //this is a synthetic event i.e preventDefault
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/createuser", {
-    // const response = await fetch("http://192.168.49.2:30189/api/createuser", {
+    const response = await fetch('http://localhost:5000/api/createuser', {
       method: 'POST',
-      //specified the header in thunderclient
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
       },
-      //json data ko stringify krke bhejte hai kyuki yahi tarika hota hai
-      body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, location: credentials.geolocation }),
+      body: JSON.stringify({
+        name: credentials.name,
+        email: credentials.email,
+        password: credentials.password,
+        location: credentials.geolocation
+      }),
       mode: 'cors'
     });
 
-    const json = await response.json()
+    const json = await response.json();
     console.log(json);
 
     if (!json.success) {
-      alert("enter valid credentials")
+      alert('Please enter valid credentials');
+    } else {
+      navigate('/Login');
     }
-    else{
-      navigate("/Login");
-    }
-
-  }
+  };
 
   const onChange = (event) => {
-    setCredentials({ ...credentials, [event.target.name]: event.target.value })
-  }
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
 
   return (
     <>
-      <div className='container'>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">Name</label>
-            <input type="text" className="form-control" name='name' value={credentials.name} onChange={onChange} />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" name='password' value={credentials.password} onChange={onChange} id="exampleInputPassword1" />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Address</label>
-            <input type="text" className="form-control" name='geolocation' value={credentials.geolocation} onChange={onChange} id="exampleInputPassword1" />
-          </div>
-          <button type="submit" className="m-3 btn btn-success">Submit</button>
-          <Link to="/Login" className='m-3 btn btn-danger'>Already a User</Link>
-        </form>
 
+      <div className='signup-container'>
+
+        <div className='signup-form-container'>
+          <h1 style={{ textAlign: 'center' }}>Create User</h1>
+          <form onSubmit={handleSubmit}>
+            <div className='form-group'>
+              <label htmlFor='name' className='form-label'>
+                Name
+              </label>
+              <input
+                type='text'
+                className='form-control'
+                name='name'
+                value={credentials.name}
+                onChange={onChange}
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='exampleInputEmail1' className='form-label'>
+                Email address
+              </label>
+              <input
+                type='email'
+                className='form-control'
+                name='email'
+                value={credentials.email}
+                onChange={onChange}
+                id='exampleInputEmail1'
+                aria-describedby='emailHelp'
+              />
+              <div id='emailHelp' className='form-text'>
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className='form-group'>
+              <label htmlFor='exampleInputPassword1' className='form-label'>
+                Password
+              </label>
+              <input
+                type='password'
+                className='form-control'
+                name='password'
+                value={credentials.password}
+                onChange={onChange}
+                id='exampleInputPassword1'
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='exampleInputPassword1' className='form-label'>
+                Address
+              </label>
+              <input
+                type='text'
+                className='form-control'
+                name='geolocation'
+                value={credentials.geolocation}
+                onChange={onChange}
+                id='exampleInputPassword1'
+              />
+            </div>
+            <button type='submit' className='mt-3  btn btn-success' style={{ marginRight: "12px" }}>
+              Sign Up
+            </button>
+            <Link to='/Login' className='mt-3 btn btn-secondary'>
+              Already a User
+            </Link>
+
+          </form>
+        </div>
       </div>
     </>
-  )
+  );
 }
