0177385850507f3356ba67af5d000974e05e67ee
ramrodwal <rrodwal22@gmail.com>
2023-04-24 20:35:39 +0530

    kubernetes

--- mernapp/src/screens/Login.js
+++ mernapp/src/screens/Login.js
@@ -1,127 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  const [credentials, setCredentials] = useState({ email: '', password: '' });
-  const navigate = useNavigate();
+
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  let navigate = useNavigate();
 
   const handleSubmit = async (e) => {
+    //this is a synthetic event i.e preventDefault
     e.preventDefault();
-
-    const response = await fetch('http://192.168.49.2:30189/api/loginuser', {
+    // const response = await fetch("http://localhost:5000/api/loginuser", {
+    const response = await fetch("http://192.168.49.2:30189/api/loginuser", {
       method: 'POST',
+      //specified the header in thunderclient
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': '*'
       },
-      body: JSON.stringify({ email: credentials.email, password: credentials.password }),
+      //json data ko stringify krke bhejte hai kyuki yahi tarika hota hai
+      body: JSON.stringify({ email: credentials.email, password: credentials.password })
     });
 
-    const json = await response.json();
+    const json = await response.json()
     console.log(json);
 
     if (!json.success) {
-      alert('Invalid credentials. Please try again.');
-    } else {
-      localStorage.setItem('userEmail', credentials.email);
-      localStorage.setItem('authToken', json.authToken);
-      console.log(localStorage.getItem('authToken'));
-      navigate('/');
+      alert("enter valid credentials")
     }
-  };
+    else{
+      localStorage.setItem("userEmail", credentials.email);
+      localStorage.setItem("authToken", json.authToken);
+      console.log(localStorage.getItem("authToken"));
+      navigate("/");
+    }
+
+  }
 
   const onChange = (event) => {
-    setCredentials({ ...credentials, [event.target.name]: event.target.value });
-  };
+    setCredentials({ ...credentials, [event.target.name]: event.target.value })
+  }
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      backgroundImage: "url('https://img.freepik.com/premium-photo/paper-bag-with-healthy-food-healthy-food-background-supermarket-food-concept-shopping-supermarket-home-delivery_167368-269.jpg?w=2000')",
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}>
-      <form onSubmit={handleSubmit} style={{
-        width: '350px',
-        padding: '30px',
-        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-        background: '#ffffff',
-      }}>
-        <h1 style={{
-          marginBottom: '20px',
-          textAlign: 'center',
-          color: '#333333',
-          fontSize: '24px',
-        }}>Log In</h1>
-        <div style={{ marginBottom: '20px' }}>
-          <label htmlFor="email" style={{
-            display: 'block',
-            marginBottom: '5px',
-            color: '#666666',
-            fontSize: '14px',
-          }}>Email Address</label>
-          <input
-            type="email"
-            style={{
-              width: '100%',
-              padding: '10px',
-              border: '1px solid #cccccc',
-              borderRadius: '5px',
-              fontSize: '14px',
-            }}
-            id="email"
-            name="email"
-            value={credentials.email}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div style={{ marginBottom: '20px' }}>
-          <label htmlFor="password" style={{
-            display: 'block',
-            marginBottom: '5px',
-            color: '#666666',
-            fontSize: '14px',
-          }}>Password</label>
-          <input
-            type="password"
-            style={{
-              width: '100%',
-              padding: '10px',
-              border: '1px solid #cccccc',
-              borderRadius: '5px',
-              fontSize: '14px',
-            }}
-            id="password"
-            name="password"
-            value={credentials.password}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <button type="submit" style={{
-          width: '100%',
-          padding: '10px',
-          border: 'none',
-          borderRadius: '5px',
-          background: '#4CAF50',
-          color: '#ffffff',
-          fontSize: '16px',
-          cursor: 'pointer',
-        }}>Log In</button>
-        <p style={{
-          marginTop: '10px',
-          textAlign: 'center',
-          color: '#666666',
-          fontSize: '14px',
-        }}>
-          Don't have an account? <Link to="/createuser" style={{ color: '#4CAF50', textDecoration: 'none' }}>Sign up here</Link>
-        </p>
-      </form>
+    <div>
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" name='password' value={credentials.password} onChange={onChange} id="exampleInputPassword1" />
+          </div>
+          <button type="submit" className="m-3 btn btn-success">Submit</button>
+          <Link to="/createuser" className='m-3 btn btn-danger'>I'm a new user</Link>
+        </form>
+
+      </div>
     </div>
-  );
+  )
 }
