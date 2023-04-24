7b9852054bb56d5c937dbbd21ba635197472f37b
vartika1203 <vartikach02@gmail.com>
2023-04-24 11:34:36 +0530

    added logger file

--- mernapp/backend/Routes/myorderData.js
+++ mernapp/backend/Routes/myorderData.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const Order = require('../models/Orders')
+const logger = require("../utils/logger");
 
 router.post('/myorderData', async(req, res) =>{
     try{
-        console.log('helloooooo')
+
         let myData= await Order.findOne({'email': req.body.email})
         res.json({orderData: myData})
+        logger.log({
+            level: "info",
+            message: "view my order data",
+        });
     }catch(error){
         res.send("this is Server Error", error.message)
+        logger.log({
+            level: "info",
+            message: "cannot view their ordered data",
+        });
     }
     })
 
