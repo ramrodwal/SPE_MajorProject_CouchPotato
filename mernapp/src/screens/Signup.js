29495d1a7aab02246d11eed7a1087da02263da58
ramrodwal <rrodwal22@gmail.com>
2023-05-02 15:26:54 +0530

    changes in ginup page

--- mernapp/src/screens/Signup.js
+++ mernapp/src/screens/Signup.js
@@ -1,73 +1,122 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { PORT } from '../components/Constants';
 
 export default function Signup() {
+  const navigate = useNavigate();
 
-  const navigate= useNavigate();
-
-  const [credentials, setCredentials] = useState({ name: "", email: "", password: "", geolocation: "" });
+  const [credentials, setCredentials] = useState({
+    name: '',
+    email: '',
+    password: '',
+    geolocation: '',
+  });
 
   const handleSubmit = async (e) => {
-    //this is a synthetic event i.e preventDefault
     e.preventDefault();
-    // const response = await fetch("http://localhost:5000/api/createuser", {
-    // const response = await fetch("http://192.168.49.2:30189/api/createuser", {
-      const response = await fetch(`http://192.168.58.2:${PORT}/api/createuser`, {
+
+    const response = await fetch(`http://192.168.58.2:${PORT}/api/createuser`, {
       method: 'POST',
-      //specified the header in thunderclient
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
       },
-      //json data ko stringify krke bhejte hai kyuki yahi tarika hota hai
-      body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, location: credentials.geolocation }),
-      mode: 'cors'
+      body: JSON.stringify({
+        name: credentials.name,
+        email: credentials.email,
+        password: credentials.password,
+        location: credentials.geolocation,
+      }),
+      mode: 'cors',
     });
 
-    const json = await response.json()
+    const json = await response.json();
     console.log(json);
 
     if (!json.success) {
-      alert("enter valid credentials")
+      alert('enter valid credentials');
+    } else {
+      navigate('/Login');
     }
-    else{
-      navigate("/Login");
-    }
-
-  }
+  };
 
   const onChange = (event) => {
-    setCredentials({ ...credentials, [event.target.name]: event.target.value })
-  }
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
 
   return (
-    <>
-      <div className='container'>
-        <form onSubmit={handleSubmit}>
+    <div
+      style={{
+        backgroundImage: `url("https://wallpapers.com/images/hd/grocery-items-circular-shape-31er4lglfoaqy7gb.jpg")`,
+        backgroundSize: 'cover',
+        height: '100vh',
+      }}
+    >
+      <div className="container">
+        <form onSubmit={handleSubmit} className="p-3 rounded shadow-sm">
           <div className="mb-3">
-            <label htmlFor="name" className="form-label">Name</label>
-            <input type="text" className="form-control" name='name' value={credentials.name} onChange={onChange} />
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <input type="text" className="form-control" name="name" value={credentials.name} onChange={onChange} />
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              value={credentials.email}
+              onChange={onChange}
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" name='password' value={credentials.password} onChange={onChange} id="exampleInputPassword1" />
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={credentials.password}
+              onChange={onChange}
+              id="exampleInputPassword1"
+            />
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Address</label>
-            <input type="text" className="form-control" name='geolocation' value={credentials.geolocation} onChange={onChange} id="exampleInputPassword1" />
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="geolocation"
+              value={credentials.geolocation}
+              onChange={onChange}
+              id="exampleInputPassword1"
+            />
           </div>
-          <button type="submit" className="m-3 btn btn-success">Submit</button>
-          <Link to="/Login" className='m-3 btn btn-danger'>Already a User</Link>
+          <div className="d-grid gap-2">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <div className="text-center mt-3">
+            <span>Already a user?</span>
+            <Link to="/Login" className="ms-2 link-danger">
+              Login here
+            </Link>
+          </div>
         </form>
-
       </div>
-    </>
-  )
+    </div>
+  );
 }
